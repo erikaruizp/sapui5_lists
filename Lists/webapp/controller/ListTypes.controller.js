82a9ee5ffde63dc1d95a965210535fc389178a96
erikaruizp <erikaruizp@gmail.com>
2021-04-18 22:43:25 +0000

    05_Listas_v5

--- Lists/webapp/controller/ListTypes.controller.js
+++ Lists/webapp/controller/ListTypes.controller.js
@@ -1,13 +1,15 @@
 // @ts-nocheck
 sap.ui.define([
         "sap/ui/core/mvc/Controller",
-        "sap/ui/model/json/JSONModel"
+        "sap/ui/model/json/JSONModel",
+        "sap/m/MessageToast"
 	],
 	/**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      * @param {typeof sap.ui.model.json.JSONModel} JSONModel
+     * @param {typeof sap.m.MessageToast} MessageToast
      */
-	function (Controller,JSONModel) {
+	function (Controller,JSONModel,MessageToast) {
 		"use strict";
 
         function onInit() {
@@ -24,14 +26,74 @@ sap.ui.define([
 
             return groupHeader;
         };
+        function onShowSelecItem() {
+            var standardList = this.getView().byId("standardList");
+            var selectedItems = standardList.getSelectedItems();
+            const oResourceBundle = this.getView().getModel("i18n").getResourceBundle();
+            var mensaje = oResourceBundle.getText("messageSelection");
 
+            if (selectedItems.length === 0) {
+                mensaje = oResourceBundle.getText("messageNoSelection");                
+            } else {
+                for (let index in selectedItems) {
+                    const oContext = selectedItems[index].getBindingContext().getObject();
+
+                    if (index === "0") {
+                        mensaje = mensaje + ' ';
+                    } else {
+                        mensaje = mensaje + ', ';
+                    }
+                    mensaje = mensaje + oContext.Material;
+                    
+                }
+            }
+            MessageToast.show(mensaje);
+        };
+        function onDeleteSelecItem() {
+            var standardList = this.getView().byId("standardList");
+            var selectedItems = standardList.getSelectedItems();
+            const oResourceBundle = this.getView().getModel("i18n").getResourceBundle();
+            var mensaje = oResourceBundle.getText("messageDelete");            
+
+            if (selectedItems.length === 0) {
+                mensaje = oResourceBundle.getText("messageNoSelection");                
+            } else {
+                var model = this.getView().getModel();
+                var products = model.getProperty("/Products");     
+                var arraySelec = [];                       
+                for (let index in selectedItems) {
+                    const oContext = selectedItems[index].getBindingContext().getObject();
+                    if (index === "0") {
+                        mensaje = mensaje + ' ';
+                    } else {
+                        mensaje = mensaje + ', ';
+                    }
+                    mensaje = mensaje + oContext.Material;
+                    arraySelec.push(oContext.Id);
+                }
+                products = products.filter( function(p) { return !arraySelec.includes(p.Id) } );
+                model.setProperty("/Products", products);
+                standardList.removeSelections();
+            }
+            MessageToast.show(mensaje);            
+        };
+        function onDeleteRow(oEvent) {
+            var context = oEvent.getParameter("listItem").getBindingContext();
+            var split = context.getPath().split("/");
+            var indexRow = split[split.length - 1];
+            var model = this.getView().getModel();
+            var products = model.getProperty("/Products");
+            products.splice(indexRow,1);
+            model.refresh();
+        };
         var Main = Controller.extend("logaligroup.Lists.controller.ListTypes", {});
 
         Main.prototype.onInit = onInit;                
         Main.prototype.getGroupHeader = getGroupHeader;
-        
-        return Main;
-                        
-        }
+        Main.prototype.onShowSelecItem = onShowSelecItem;
+        Main.prototype.onDeleteSelecItem = onDeleteSelecItem;
+        Main.prototype.onDeleteRow = onDeleteRow;
+        return Main;    
+    }
 );
 
